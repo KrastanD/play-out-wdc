7a0dea871fb4dbe5d5818624700918df128fc7f6
Krastan Dimitrov <krastan.kdd@gmail.com>
2021-11-21 22:38:03 -0600

    Update with Qatar results Update arrows Add link to WCC

--- src/components/Instructions/index.tsx
+++ src/components/Instructions/index.tsx
@@ -16,18 +16,16 @@ const Instructions = () => {
           <h5 className="d-sm-none text-center">
             Instructions
             <br />
-            Tap -{">"} {version === AppVersion.WDC ? "Max" : "Red Bull"}
+            Tap → {version === AppVersion.WDC ? "Max" : "Red Bull"}
             <br />
-            Long Press -{">"}{" "}
-            {version === AppVersion.WDC ? "Lewis" : "Mercedes"}
+            Long Press → {version === AppVersion.WDC ? "Lewis" : "Mercedes"}
           </h5>
           <h5 className="d-none d-sm-block text-center">
             Instructions
             <br />
-            Left Click -{">"} {version === AppVersion.WDC ? "Max" : "Red Bull"}
+            Left Click → {version === AppVersion.WDC ? "Max" : "Red Bull"}
             <br />
-            Right Click -{">"}{" "}
-            {version === AppVersion.WDC ? "Lewis" : "Mercedes"}
+            Right Click → {version === AppVersion.WDC ? "Lewis" : "Mercedes"}
           </h5>
         </div>
       </div>
