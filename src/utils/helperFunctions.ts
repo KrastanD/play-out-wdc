df0cb0fd17108fa32d18b0449d608ca8676f7245
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-01-09 23:35:50 -0600

    Added type for possible time units

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,5 +1,7 @@
 export const arrayColumn = (arr: number[][], n: number) => arr.map((x) => x[n]);
 
-export const getTimeString = (amount: number, unit: string): string => {
+type TimeUnit = "second" | "minute" | "hour" | "day" | "month" | "year"
+
+export const getTimeString = (amount: number, unit: TimeUnit): string => {
     return amount === 1 ? `${amount} ${unit}` : `${amount} ${unit}s`;
   };
