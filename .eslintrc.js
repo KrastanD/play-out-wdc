1337a6634fd96c7ac6f15ce42a59005f068413cf
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-03-26 17:06:58 -0500

    Add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "import/extensions": ["error", "never"],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "error",
+    "react/require-default-props": "off",
+  },
+};
