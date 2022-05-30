3f9e4deac4e4ad0c011728892b1d73e5d7b4d77b
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-05-30 14:17:52 -0500

    Add title to pages

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,14 @@
+import { useLocation } from "react-router-dom";
+
+import "./Title.styles.scss";
+
+function Title() {
+  const location = useLocation();
+  const path = location.pathname;
+  const championship = path.split("/")[1];
+  const year = path.split("/")[2];
+
+  return <h2 className="Title">{`${championship}' Championship ${year}`}</h2>;
+}
+
+export default Title;
