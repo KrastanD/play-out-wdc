5fcd76d51d0db9b044f2e42c755aeb033a2f0ecf
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-06-09 22:42:42 -0500

    Dropdown gets closed on click

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Dropdown.styles.scss";
 
@@ -12,22 +13,33 @@ type DropdownProps = {
 
 function Dropdown({ title, items }: DropdownProps) {
   const navigation = useNavigate();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const handleClick = (linkto: string) => {
+    navigation(linkto);
+    setShowDropdown(false);
+  };
   return (
-    <div className="Dropdown">
+    <div
+      className="Dropdown"
+      onMouseEnter={() => setShowDropdown(true)}
+      onMouseLeave={() => setShowDropdown(false)}
+    >
       {title}
-      <ul className="Dropdown__list">
-        {items.map((item) => (
-          <li key={item.text}>
-            <button
-              className="Dropdown__item"
-              onClick={() => navigation(item.linkto)}
-              type="button"
-            >
-              {item.text}
-            </button>
-          </li>
-        ))}
-      </ul>
+      {showDropdown && (
+        <ul className="Dropdown__list">
+          {items.map((item) => (
+            <li key={item.text}>
+              <button
+                className="Dropdown__item"
+                onClick={() => handleClick(item.linkto)}
+                type="button"
+              >
+                {item.text}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
