f6bb1c71991a8b5a0b2b5e035df3813572da83da
Krastan Dimitrov <krastan.kdd@gmail.com>
2025-01-20 21:07:59 -0600

    Redirect to / to /drivers/ Add ErrorBoundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
